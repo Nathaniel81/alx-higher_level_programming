f0e97b7adee434da6fa2a282f9b5e3abbd08b684
Nathaniel81 <nathanield356@gmail.com>
2022-08-23 07:58:30 -0700

    The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,6 @@ else:
     lst_d = number % 10
 msg = "Last digit of "
 if lst_d < 0:
-    lst_d = -lst_d
     print("{}{} is {} and is less than 6 and not 0".format(msg, number, lst_d))
 elif lst_d < 6 and lst_d != 0:
     print("{}{} is {} and is less than 6 and not 0".format(msg, number, lst_d))
