2843b1f98fb2934118c4d17525bbcf209e162790
Nathaniel81 <nathanield356@gmail.com>
2022-09-12 19:21:45 +0300

    Safe printing of an integers list

--- 0x05-python-exceptions/prc.py
+++ 0x05-python-exceptions/prc.py
@@ -12,7 +12,7 @@ else:
 	print(k)
 finally:
 	print('k')
-'''
+
 def safe_print_list(my_list=[], x=0):
 	c = 0
 	try:
@@ -32,3 +32,57 @@ nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
+from pickle import TRUE
+def safe_print_integer(value):
+	try:
+		print("{:d}".format(value))
+		status = True
+	except ValueError:
+		status = False
+	finally:
+		return status
+
+value = 89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+'''
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
