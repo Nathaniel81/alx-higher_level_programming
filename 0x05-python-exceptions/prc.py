b68065f8238f8e76fb160be5c37ba81c288f523a
Nathaniel81 <nathanield356@gmail.com>
2022-09-12 20:19:26 +0300

    add prc

--- 0x05-python-exceptions/prc.py
+++ 0x05-python-exceptions/prc.py
@@ -56,8 +56,55 @@ value = "School"
 has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
+
+from itertools import count
+
+def safe_print_list_integers(my_list=[], x=0):
+	count = 0
+	for i in range(x):
+		try:
+			print("{:d}".format(my_list[i]), end='')
+			count += 1
+		except (ValueError, TypeError):
+			continue
+		except IndexError:
+			pass
+	print()
+	return count
+		#return count
+		#print()
+		#pass
+	#finally:
+	#return count
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
 '''
-		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
