4930a33d254874b83e7d4ee706b8187e171ea283
Nathaniel81 <nathanield356@gmail.com>
2022-09-12 19:23:23 +0300

    TypeError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         status = True
-    except ValueError:
+    except (ValueError, TypeError):
         status = False
     finally:
         return status
