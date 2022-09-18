e57b417115851ff1156b668dda0e43e93c38e6b8
Nathaniel81 <nathanield356@gmail.com>
2022-09-18 07:32:34 -0700

    Print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,14 +3,14 @@
 
 
 def print_square(size):
-	"""Prints a square of length size"""
+    """Prints a square of length size"""
 
-if isinstance(size, int):
-if size >= 0:
-for i in range(size):
-[print('#', end='') for i in range(size)]
-print()
-else:
-raise ValueError("size must be >= 0")
-else:
-raise TypeError("size must be an integer")
+    if isinstance(size, int):
+        if size >= 0:
+            for i in range(size):
+                [print('#', end='') for i in range(size)]
+                print()
+        else:
+            raise ValueError("size must be >= 0")
+    else:
+        raise TypeError("size must be an integer")
