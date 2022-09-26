ea34bff75a85f4f0e8d7d5af58ffb30b237df0cb
Nathaniel81 <nathanield356@gmail.com>
2022-09-26 16:11:24 +0300

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Documentaion for print_sorted function"""
+
+
+class MyList(list):
+    """Inherits from the list class"""
+
+    def __init__(self):
+        """Initializing this class with the parrent init method"""
+        list.__init__(self)
+
+    def print_sorted(self):
+        """Prints the list in ascending order"""
+        print(sorted(self))
