647c0cd54200e4f239f0fe84ec2507a42fce0737
Nathaniel81 <nathanield356@gmail.com>
2022-09-26 15:09:03 +0300

    Look up

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Documentation for a function that returns the list of objects"""
+
+
+def lookup(obj):
+    """Returns the list of available attributes"""
+    #return obj.__dict__
+    #return vars(obj)
+    
+    return dir(obj)
