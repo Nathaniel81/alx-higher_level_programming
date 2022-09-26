058824c18d154b804b847943f7c262ab006be472
Nathaniel81 <nathanield356@gmail.com>
2022-09-26 17:22:34 +0300

    Only sub class of

---
+++ 0x0A-python-inheritance/4-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+inherits_from = __import__('4-inherits_from').inherits_from
+def inherits_from(obj, a_class):
+    """ returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False."""
+
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
