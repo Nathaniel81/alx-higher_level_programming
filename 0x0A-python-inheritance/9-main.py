1bf6ac837bf7eff56b3e9e6c60c34bc86ef66f6e
Nathaniel81 <nathanield356@gmail.com>
2022-09-26 23:00:23 +0300

    Full rectangle

---
+++ 0x0A-python-inheritance/9-main.py
@@ -0,0 +1,19 @@
+"""#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle"""
+BR = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BR):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+    def area(self):
+        return self.__width * self.__height
+    def __str__(self):
+        return "[{}] {}/{}".format(self.__class__.__name__, self.__width, self.__height)
+    
+r = Rectangle(3, 5)
+
+print(r)
+print(r.area())
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Documentation for a rectangle class"""
+
+
+BR = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BR):
+    """Represents a Rectangle"""
+
+    def __init__(self, width, height):
+        """Instantiate the class"""
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+        
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
+    def __str__(self):
+        return "[{}] {}/{}".format(self.__class__.__name__, self.__width, self.__height)
