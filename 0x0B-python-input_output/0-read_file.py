32be1228cd547b7c0b591667b805bc8565b8e803
Nathaniel81 <nathanield356@gmail.com>
2022-09-28 05:48:10 -0700

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """ reads a text file (UTF8) and prints it to stdout"""
 
-    with open("my_file_0.txt", "r", encoding="utf-8") as m:
-        print(m.read())
+    with open("my_file_0.txt", "r", encoding="utf-8") as file:
+        print(file.read(), end='')
