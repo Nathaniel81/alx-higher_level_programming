0186dfe01794f9058d429c3d205c0079a04aae4e
Nathaniel81 <nathanield356@gmail.com>
2022-09-29 00:36:34 +0300

    Class to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defining a student class"""
+
+
+class Student:
+    """A student class"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """Returns the dictionary description
+        for JSON serialization of an object"""
+        return vars(self)
