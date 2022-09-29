92caeab737ef7a462f30fce6b04738688a09745c
Nathaniel81 <nathanield356@gmail.com>
2022-09-29 16:50:55 +0300

    Student to disk and reload

--- 0x0B-python-input_output/prc.py
+++ 0x0B-python-input_output/prc.py
@@ -11,3 +11,16 @@ dictionary = {
 with open("sample.json", "w") as outfile:
 	json.dump(dictionary, outfile)
 """
+class A:
+    def __init__(self, name, val):
+        self.name = name
+        self.val = val
+    def  setttt(self, dic):
+        for i in dic.keys():
+            if i in self.__dict__.keys():
+                self.__dict__[i] = dic[i]
+
+x = A('Ab', 45)
+dic1 = {'name': 'bb'}
+x.setttt(dic1)
+print(x.name)
