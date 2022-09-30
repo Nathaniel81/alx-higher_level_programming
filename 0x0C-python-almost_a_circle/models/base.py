1f640ef4684aff793cd9cd128fa0cebc1b22c3cd
Nathaniel81 <nathanield356@gmail.com>
2022-09-30 20:44:35 +0300

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Documentation for a single class called Base"""
+
+
+class Base:
+    """The “base” of all other classes"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Instatntiation"""
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
