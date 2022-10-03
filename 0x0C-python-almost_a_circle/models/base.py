4cc18058b56e154b9bb80ec361ed7df93793b254
Nathaniel81 <nathanield356@gmail.com>
2022-10-03 19:03:57 +0300

    CSV

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """ This module creats the base class of all other classes """
 
 
+import csv
 import json
 class Base:
     """ The base of all other classes """
@@ -62,5 +63,37 @@ class Base:
             with open(filename, "r") as jsonfile:
                 list_dicts = Base.from_json_string(jsonfile.read())
                 return [cls.create(**inst) for inst in list_dicts]
+        except IOError:
+            return []
+
+    def save_to_file_csv(cls, list_objs):        
+        """ serializes and deserializes in CSV """
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline="") as csvfile:
+            if list_objs is None or list_objs == []:
+                csvfile.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ Returns a list of classes instantiated from a CSV file """
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [dict([k, int(v)] for k, v in d.items())
+                              for d in list_dicts]
+                return [cls.create(**d) for d in list_dicts]
         except IOError:
             return []
--- 0x0C-python-almost_a_circle/r.py
+++ 0x0C-python-almost_a_circle/r.py
@@ -1,4 +1,4 @@
-class Rectangle:
+"""class Rectangle:
     def __init__(self, val, value):
         self.val = val
         self.value = value
@@ -10,3 +10,23 @@ r2 = Rectangle(2, 4)
 list_rectangles_input = [r1, r2]
 for i in list_rectangles_input:
     print(i)
+"""
+import csv
+
+#with open("x.csv", 'r') as f:
+#    mcs = csv.reader(f)
+#    for i in mcs:
+#        print(i)
+#    with open("xx.csv", 'w') as ff:
+#        mcs2 = csv.writer(ff, delimiter='\t ')
+#        for i in mcs:
+#            mcs2.writerow(i)
+
+with open("x.csv", 'r') as f:
+    mcs = csv.DictReader(f)
+    with open("xx.csv", 'w') as ff:
+        fld = ['Name', 'Sex', 'Age']
+        mcs2 = csv.DictWriter(ff, fieldnames=fld, delimiter='\t')
+        mcs2.writeheader()
+        for i in mcs:
+            mcs2.writerow(i)
