8be5b8142e9b8aafa7833e17d82790205eb1d657
Nathaniel81 <nathanield356@gmail.com>
2022-11-09 22:02:40 -0800

    9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- Creates a table second_table in the database hbtn_0c_0
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT --AUTO_INCREMENT,
+	id INT, --AUTO_INCREMENT
 	name VARCHAR(256),
-	score INT,
+	score INT
 	--PRIMARY KEY (id)
 );
 INSERT INTO second_table(id, name, score)
