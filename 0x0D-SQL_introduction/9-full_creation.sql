27fb926da0fe4df0460c3056975a6fceb1276051
Nathaniel81 <nathanield356@gmail.com>
2022-11-09 22:06:29 -0800

    9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS second_table (
 	--PRIMARY KEY (id)
 );
 INSERT INTO second_table(id, name, score)
-VALUES(1 'Jhon', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+VALUES(1 "Jhon", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
