2ae8c0b4d37bcfc2041b4253fd520760b90ff604
Nathaniel81 <nathanield356@gmail.com>
2022-11-12 10:21:06 -0800

    13 - 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Lists all shows, and all genres linked to that show.
+SELECT tv_shows.title, tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title;
