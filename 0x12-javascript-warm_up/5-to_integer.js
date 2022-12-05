caba6025d15f63470e0cc37f94b736106bbe255f
Nathaniel81 <nathanield356@gmail.com>
2022-12-05 16:24:51 +0300

    An int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const argv = process.argv;
 
-if (isNaN(argv[2]) || !argv[2]) {
+if (isNaN(parseInt(argv[2])) || !argv[2]) {
 	console.log('Not a number');
 } else {
 	console.log('My Number: ' + argv[2]);
