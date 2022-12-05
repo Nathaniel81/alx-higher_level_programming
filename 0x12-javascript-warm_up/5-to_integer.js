ccedd8165b145763a01beead854150ef167ad39a
Nathaniel81 <nathanield356@gmail.com>
2022-12-05 16:01:39 +0300

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const argv = process.argv;
 
 if (parseInt(argv[2])) {
-	console.log('My Number: ' + argv[2]);
+	console.log('My Number: ' + parseInt(argv[2]));
 } else {
 	console.log('Not a number');
 }
