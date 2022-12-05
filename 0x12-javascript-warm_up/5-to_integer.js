2f885cc58b5dae98101dc5946ea9a0d9529a1e40
Nathaniel81 <nathanield356@gmail.com>
2022-12-05 16:35:15 +0300

    An int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
-const myVar = parseInt(process.argv[2]);
-if (process.argv[2] === undefined || isNaN(myVar)) {
-  console.log('Not a number');
+const Argv = parseInt(process.argv[2]);
+
+if (process.argv[2] === undefined || isNaN(Argv)) {
+	console.log('Not a number');
 } else {
-  console.log('My number: ' + myVar);
+	console.log('My Number: ' + Argv);
 }
