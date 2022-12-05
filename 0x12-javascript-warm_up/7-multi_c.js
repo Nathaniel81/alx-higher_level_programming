be445b86897b036113c9c576135084b96129cc14
Nathaniel81 <nathanield356@gmail.com>
2022-12-05 21:48:45 +0300

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 const argv = process.argv[2];
-
 if (isNaN(argv)) {
   console.log('Missing number of occurrences');
 } else {
-for (let i = 0; i < argv; i++) console.log('C is fun');
+for (let i = 0; i < argv; i++) { 
+  console.log('C is fun');
+}
 }
