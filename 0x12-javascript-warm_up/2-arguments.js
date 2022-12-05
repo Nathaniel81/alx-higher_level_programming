84ef5c1481b9fd13bc7f1ce2a4fd09d0102092a0
Nathaniel81 <nathanield356@gmail.com>
2022-12-05 02:41:30 -0800

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,4 +4,6 @@ if (process.argv.length >= 3){
 	console.log('Argument passed');
 } else if (process.argv.length < 3){
 	console.log('No arguments');
-}
+} else {
+	console.log('Arguments found');
+}	
