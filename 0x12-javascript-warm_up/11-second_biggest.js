4eeddfc16efba9c8df042d6a7335b6ea76a10f61
Nathaniel81 <nathanield356@gmail.com>
2022-12-06 09:30:42 +0300

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,8 +8,8 @@ if (args.length < 4) {
 	console.log(0);
 } else {
 	for (let i = 3; i < args.length; i++) {
-		if (fstbig < args[i]){
-			fstbig = args[i];
+		if (fstbig < parseInt(args[i])){
+			fstbig = parseInt(args[i]);
 			idx = i;
 		}
 	}
@@ -17,8 +17,8 @@ if (args.length < 4) {
 	let scndbig = parseInt(args[2]);
 
 	for (let i = 3; i < args.length; i++) {
-		if (scndbig < args[i]){
-			scndbig = args[i];
+		if (scndbig < parseInt(args[i])){
+			scndbig = parseInt(args[i]);
 		}
 	}
 	console.log(scndbig);
