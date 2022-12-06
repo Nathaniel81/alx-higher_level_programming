11417e7ab8e505075396d6c243bc1579bec08815
Nathaniel81 <nathanield356@gmail.com>
2022-12-06 14:12:42 +0300

    Semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,21 +5,21 @@ let fstbig = parseInt(args[2]);
 let idx = 2;
 
 if (args.length < 4) {
-	console.log(0);
+  console.log(0);
 } else {
-	for (let i = 3; i < args.length; i++) {
-		if (fstbig < parseInt(args[i])){
-			fstbig = parseInt(args[i]);
-			idx = i;
-		}
-	}
-	args.splice(idx, 1);
-	let scndbig = parseInt(args[2]);
+  for (let i = 3; i < args.length; i++) {
+    if (fstbig < parseInt(args[i])) {
+      fstbig = parseInt(args[i]);
+        idx = i;
+    }
+  }
+  args.splice(idx, 1);
+  let scndbig = parseInt(args[2]);
 
-	for (let i = 3; i < args.length; i++) {
-		if (scndbig < parseInt(args[i])){
-			scndbig = parseInt(args[i]);
-		}
-	}
-	console.log(scndbig);
-	}
+  for (let i = 3; i < args.length; i++) {
+    if (scndbig < parseInt(args[i])){
+      scndbig = parseInt(args[i]);
+    }
+  }
+  console.log(scndbig);
+}
