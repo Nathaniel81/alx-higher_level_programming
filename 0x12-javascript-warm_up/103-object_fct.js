3414babc47209c3f3ecd8f2f2043e1dd9cee75ee
Nathaniel81 <nathanield356@gmail.com>
2022-12-06 13:58:28 +0300

    Increment object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-  myObject.incr = () => {
-	myObject.value++;
-  };
+myObject.incr = () => {
+  myObject.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
