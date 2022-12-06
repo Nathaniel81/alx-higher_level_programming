fa9eb481c35cdee4aafd08a96aa5eb8edc1e6261
Nathaniel81 <nathanield356@gmail.com>
2022-12-06 14:48:45 +0300

    Semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,15 +3,15 @@
 function factorial (n) {
   if (n === 0) {
     return (1);
-	} else {
-      return (n * factorial(n - 1));
-	}
+  } else {
+    return (n * factorial(n - 1));
+  }
 }
-  
+
 const argv = process.argv;
 const num = parseInt(argv[2]);
 if (isNaN(num)) {
   console.log('1');
-  } else {
-      console.log(factorial(num));
+} else {
+  console.log(factorial(num));
 }
