f70b1b765568813a890b9fd0b490617a617e58d2
Nathaniel81 <nathanield356@gmail.com>
2022-12-08 13:01:21 +0300

    8 - 13

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const theList = require('./100-data').list;
-
-const newList = theList.map(lst => lst * (theList.indexOf(lst)));
-console.log(theList);
-console.log(newList);
+const list = require('./100-data').list;
+console.log(list);
+console.log(list.map((element, index) => element * index));
