89d339c5bc874a231827cf03ab1974d6fee60052
Nathaniel81 <nathanield356@gmail.com>
2022-12-15 11:06:21 +0300

    City relationship

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -3,14 +3,16 @@
 """Doc"""
 
 from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
-Base = declarative_base
+Base = declarative_base()
 
 
 class City(Base):
-    """Doc"""
+    """
+    Doc
+    """
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -3,12 +3,14 @@
 
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
-from relationship_city import Base
+from relationship_city import City, Base
 
 
 class State(Base):
-    """Doc"""
+    """
+    Doc
+    """
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
