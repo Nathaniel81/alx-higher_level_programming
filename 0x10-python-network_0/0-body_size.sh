0a19df82cd5474bfc2e64a62029bd0d4ff353722
Nathaniel81 <nathanield356@gmail.com>
2022-12-23 00:00:36 -0800

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Doc
 
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
+#curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
