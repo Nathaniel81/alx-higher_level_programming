1d1d8610539e15d96e792b24f14467975ac5c72d
Nathaniel81 <nathanield356@gmail.com>
2022-12-24 09:56:26 -0800

    Only 17%

--- 0x11-python-network_1/11.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from sys import argv
-from requests import get
-
-result = get("https://api.github.com/repos/{}/{}/commits".format(
-    argv[2], argv[1]))
-commits = result.json()
-print(commits)
