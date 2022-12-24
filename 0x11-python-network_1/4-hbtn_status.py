86425ba745e9d2ac8de7b245a5d955f119c89004
Nathaniel81 <nathanield356@gmail.com>
2022-12-24 14:56:42 +0300

    Fetch using the request package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status
+using the request package"""
+
+import requests
+
+if __name__ == '__main__':
+    response = requests.get('https://alx-intranet.hbtn.io/status').text
+    print("Body response: ")
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
